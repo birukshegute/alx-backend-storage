0ee014d507eda10e1f66a94597cf5e169a1b9b4a
birukshegute <birukshegute@gmail.com>
2024-07-17 19:39:06 +0000

    mandatory

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""
+project 0x02 redis basic project mandatory tasks
+"""
+
+import redis
+from uuid import uuid4
+from functools import wraps
+from typing import Any, Callable, Optional, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    """ 
+    Counts calls
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """
+        Wraps called method
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    records hidtory
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args) -> str:
+        """
+        Wraps called method
+        """
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
+        output = method(self, *args)
+        self._redis.rpush(f'{method.__qualname__}:outputs', output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable) -> None:
+    """
+    Shows how many times a function was called.
+    """
+    client = redis.Redis()
+    calls = client.get(fn.__qualname__).decode('utf-8')
+    inputs = [input.decode('utf-8') for input in
+              client.lrange(f'{fn.__qualname__}:inputs', 0, -1)]
+    outputs = [output.decode('utf-8') for output in
+               client.lrange(f'{fn.__qualname__}:outputs', 0, -1)]
+    print(f'{fn.__qualname__} was called {calls} times:')
+    for input, output in zip(inputs, outputs):
+        print(f'{fn.__qualname__}(*{input}) -> {output}')
+
+
+class Cache:
+    """
+    Class for Cache
+    """
+    def __init__(self) -> None:
+        """
+        Initialize a new obj
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
+        """
+        Method to store data
+        """
+        key = str(uuid4())
+        client = self._redis
+        client.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+        Method to get data
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """
+        Method to convert to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        Method to convert to integer
+        """
+        return int(data)
