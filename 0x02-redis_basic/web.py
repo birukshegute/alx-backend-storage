216bc92fea3b2c3d0ab528bf93c9bd63c4e062f7
birukshegute <birukshegute@gmail.com>
2024-07-18 07:21:09 +0000

    advanced edited

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,30 +9,35 @@ from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """
-    Tracks get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """
-        Tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+redis_store = redis.Redis()
+"""
+Redis instance for module.
+"""
 
 
-@track_get_page
+def data_cacher(method: Callable) -> Callable:
+    """
+    Output of fetched data is cached
+    """
+    @wraps(method)
+    def invoker(url) -> str:
+        """
+        The wrapper method for caching
+        """
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
     """
     Obtain the HTML content of a particular URL
     """
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
